5a64e4d126f5e3e86cd20a0d03b672d07acc0531
wmclaug2 <wmclaug2@gmail.com>
2019-01-12 14:29:45 -0500

    Updates

--- precache-manifest.381e2e9e017a1e9f0f57bd57c038c4e9.js
+++ precache-manifest.381e2e9e017a1e9f0f57bd57c038c4e9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "faff92145777a3cbaf8e7367b4807987",
-    "url": "/sleaverva/static/media/icons.faff9214.woff"
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/sleaverva/static/media/icons.0ab54153.woff2"
   },
   {
-    "revision": "438c6b22e790f12c30b5",
+    "revision": "d511a56041f69003310a",
     "url": "/sleaverva/static/css/main.f801283c.chunk.css"
   },
   {
@@ -56,24 +56,16 @@ self.__precacheManifest = [
     "url": "/sleaverva/static/media/skyline.73f2f5a1.jpg"
   },
   {
-    "revision": "9c74e172f87984c48ddf5c8108cabe67",
-    "url": "/sleaverva/static/media/flags.9c74e172.png"
-  },
-  {
-    "revision": "438c6b22e790f12c30b5",
-    "url": "/sleaverva/static/js/main.438c6b22.chunk.js"
-  },
-  {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "/sleaverva/static/media/icons.b87b9ba5.ttf"
-  },
-  {
-    "revision": "701ae6abd4719e9c2ada3535a497b341",
-    "url": "/sleaverva/static/media/outline-icons.701ae6ab.eot"
-  },
-  {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
     "url": "/sleaverva/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "d511a56041f69003310a",
+    "url": "/sleaverva/static/js/main.d511a560.chunk.js"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/sleaverva/static/media/icons.faff9214.woff"
   },
   {
     "revision": "cd6c777f1945164224dee082abaea03a",
@@ -84,12 +76,24 @@ self.__precacheManifest = [
     "url": "/sleaverva/static/media/outline-icons.ef60a4f6.woff"
   },
   {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/sleaverva/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/sleaverva/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
     "revision": "ad97afd3337e8cda302d10ff5a4026b8",
     "url": "/sleaverva/static/media/outline-icons.ad97afd3.ttf"
   },
   {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/sleaverva/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/sleaverva/static/media/icons.962a1bf3.svg"
   },
   {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
@@ -100,27 +104,23 @@ self.__precacheManifest = [
     "url": "/sleaverva/static/media/brand-icons.e8c322de.woff2"
   },
   {
-    "revision": "962a1bf31c081691065fe333d9fa8105",
-    "url": "/sleaverva/static/media/icons.962a1bf3.svg"
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/sleaverva/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/sleaverva/static/media/flags.9c74e172.png"
   },
   {
     "revision": "a046592bac8f2fd96e994733faf3858c",
     "url": "/sleaverva/static/media/brand-icons.a046592b.woff"
   },
   {
-    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
-    "url": "/sleaverva/static/media/brand-icons.c5ebe0b3.ttf"
-  },
-  {
-    "revision": "0ab54153eeeca0ce03978cc463b257f7",
-    "url": "/sleaverva/static/media/icons.0ab54153.woff2"
-  },
-  {
     "revision": "7ba589dd441d98677913",
     "url": "/sleaverva/static/css/1.ef7468c2.chunk.css"
   },
   {
-    "revision": "a50dba2c1f6ceed0da638d8ea81a2b28",
+    "revision": "4375cbdb268fd20def94679c62b41b4d",
     "url": "/sleaverva/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sleaverva/precache-manifest.170ce7d5db1678521257e0f9423cb34f.js"
+  "/sleaverva/precache-manifest.381e2e9e017a1e9f0f57bd57c038c4e9.js"
 );
 
 workbox.clientsClaim();
