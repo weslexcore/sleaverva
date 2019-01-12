1373a0f74db6650858c244174d4adf02d3dfd831
wmclaug2 <wmclaug2@gmail.com>
2019-01-12 10:57:52 -0500

    Updates

---
+++ precache-manifest.1eb6d12f95fa5150c354b44a9e45946c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "845598d8013c669f2f5a",
+    "url": "/sleaverva/static/js/runtime~main.845598d8.js"
+  },
+  {
+    "revision": "744b4faf518ca9790b3c",
+    "url": "/sleaverva/static/js/main.744b4faf.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/sleaverva/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "7f92f6d1ff06c8f756ba8ba0b739159d",
+    "url": "/sleaverva/index.html"
+  }
+];
