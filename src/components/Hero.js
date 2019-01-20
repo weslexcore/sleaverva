530d0d6fa2478fb7037f4217772edb88c1310e58
wmclaug2 <wmclaug2@gmail.com>
2019-01-20 13:11:07 -0500

    Updates to hero font

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,8 +11,8 @@ const Hero = props => {
             {props.children}
             <SocialLinks inverted style={{position:'absolute', display:'flex', top:15, right:10, zIndex: 30}}/>
             <div style={{display:'flex', justifyContent:'center', alignItems:'center', position:'absolute', width:'100vw', height:'100vh', backgroundColor:'rgba(0,0,0,.5)', padding:10, textAlign:'center'}}>
-                <Header className='logo' style={{fontSize:'10rem', color:'#fafafa'}}>SLEAVE</Header>
-                <Header className='logo' style={{fontSize:'5rem', color:'#fafafa'}}>RVA</Header>
+                <Header className='logo' style={{fontSize:'12rem', color:'#fafafa'}}>SLEAVE</Header>
+                {/* <Header className='logo' style={{fontSize:'5rem', color:'#fafafa'}}>RVA</Header> */}
             </div>
         </ParallaxBackground>
     )
