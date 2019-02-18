8d72b81040d52dbc340f133a74ae8c045c5b2cd0
wmclaug2 <wmclaug2@gmail.com>
2019-02-18 11:53:05 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sleaverva/precache-manifest.7fa4b95a4e53e865f9140aef250b181a.js"
+  "/sleaverva/precache-manifest.9388fc625280f9b785b6099fcf46121b.js"
 );
 
 workbox.clientsClaim();
