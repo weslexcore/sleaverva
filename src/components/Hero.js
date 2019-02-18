7b477b7a39e515a3d9c76918a5ba76ed72028e99
wmclaug2 <wmclaug2@gmail.com>
2019-02-18 12:04:14 -0500

    Updates

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {  Header } from 'semantic-ui-react'
 import ParallaxBackground from './ParallaxBackground'
 import SocialLinks from './SocialLinks'
-import pink from '../assets/img/pink.jpg'
+import pink from '../assets/img/pink_cropped.jpg'
 
 
 const Hero = props => {
