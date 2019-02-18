162a6875a3c28fadd5223cda1587ce736c3347c6
wmclaug2 <wmclaug2@gmail.com>
2019-02-18 12:52:00 -0500

    Updates

---
+++ precache-manifest.336b4c282bc3d2d21a1532937b4e627c.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb67b6405523b46eb8d3",
+    "url": "/static/css/main.f801283c.chunk.css"
+  },
+  {
+    "revision": "fb67b6405523b46eb8d3",
+    "url": "/static/js/main.fb67b640.chunk.js"
+  },
+  {
+    "revision": "75ffccb41f8d84329e5d",
+    "url": "/static/css/1.276c3cc5.chunk.css"
+  },
+  {
+    "revision": "75ffccb41f8d84329e5d",
+    "url": "/static/js/1.75ffccb4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6bdbb3043de70c03ad65d2df922c3c93",
+    "url": "/static/media/fb.6bdbb304.png"
+  },
+  {
+    "revision": "43f80c6e8a09eae43898fceac5072be4",
+    "url": "/static/media/bandcamp.43f80c6e.png"
+  },
+  {
+    "revision": "ac0f460641ca274f367364125cf2714c",
+    "url": "/static/media/bandcampwhite.ac0f4606.png"
+  },
+  {
+    "revision": "924e11ef5f7311bb361f6cedf18daccd",
+    "url": "/static/media/pink_cropped.924e11ef.jpg"
+  },
+  {
+    "revision": "ce6e48a439ec001a412be3921e12ab03",
+    "url": "/static/media/gold.ce6e48a4.jpg"
+  },
+  {
+    "revision": "33767c5ed2d40179b258c660312b2d24",
+    "url": "/static/media/bandPink.33767c5e.jpg"
+  },
+  {
+    "revision": "b743e63f49ab4225d1735be75be68a32",
+    "url": "/static/media/cuts.b743e63f.jpg"
+  },
+  {
+    "revision": "a1b9eefeec83fb339429eb928e2448cc",
+    "url": "/static/media/jump.a1b9eefe.jpg"
+  },
+  {
+    "revision": "73f2f5a14190dbe00855a59161f60c95",
+    "url": "/static/media/skyline.73f2f5a1.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "eb7eb13cf58881d715e3f0700fc74041",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "sleave.band/precache-manifest.d95f21da888598ffd2a768f8f8258e50.js"
+  "/precache-manifest.336b4c282bc3d2d21a1532937b4e627c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("sleave.band/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
