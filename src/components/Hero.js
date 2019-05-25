b25114e22fd2e208470ad79ca9d56ad3f36afb54
wmclaug2 <wmclaug2@gmail.com>
2019-05-25 10:46:23 -0400

    Font updates and add analytics

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import {  Header } from 'semantic-ui-react'
 import ParallaxBackground from './ParallaxBackground'
@@ -11,7 +12,7 @@ const Hero = props => {
             {props.children}
             <SocialLinks inverted style={{position:'absolute', display:'flex', top:15, right:10, zIndex: 30}}/>
             <div style={{display:'flex', justifyContent:'center', alignItems:'center', position:'absolute', width:'100vw', height:'100vh', backgroundColor:'rgba(0,0,0,.5)', padding:10, textAlign:'center'}}>
-                <Header className='logo' style={{fontSize:'12rem', color:'#fafafa'}}>SLEAVE</Header>
+                <Header className='logo' style={{fontSize:'9rem', color:'#fafafa'}}>SLEAVE</Header>
                 {/* <Header className='logo' style={{fontSize:'5rem', color:'#fafafa'}}>RVA</Header> */}
             </div>
         </ParallaxBackground>
