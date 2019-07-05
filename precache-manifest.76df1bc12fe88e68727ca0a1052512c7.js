36354df71cb0b3ed02b29378d417f807cd8595f8
wmclaug2 <wmclaug2@gmail.com>
2019-07-04 23:14:10 -0400

    Updates

--- precache-manifest.76df1bc12fe88e68727ca0a1052512c7.js
+++ precache-manifest.76df1bc12fe88e68727ca0a1052512c7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "bb03149f75f2e75c34b9",
+    "revision": "23c9ed6458aa2a39c2b0",
     "url": "/static/css/main.719d29e8.chunk.css"
   },
   {
-    "revision": "bb03149f75f2e75c34b9",
-    "url": "/static/js/main.7bff28cd.chunk.js"
+    "revision": "23c9ed6458aa2a39c2b0",
+    "url": "/static/js/main.04be0ece.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -64,16 +64,16 @@ self.__precacheManifest = [
     "url": "/static/media/outline-icons.ef60a4f6.woff"
   },
   {
-    "revision": "701ae6abd4719e9c2ada3535a497b341",
-    "url": "/static/media/outline-icons.701ae6ab.eot"
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
   },
   {
     "revision": "ad97afd3337e8cda302d10ff5a4026b8",
     "url": "/static/media/outline-icons.ad97afd3.ttf"
   },
   {
-    "revision": "9c74e172f87984c48ddf5c8108cabe67",
-    "url": "/static/media/flags.9c74e172.png"
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
   },
   {
     "revision": "0ab54153eeeca0ce03978cc463b257f7",
@@ -100,12 +100,12 @@ self.__precacheManifest = [
     "url": "/static/media/icons.b87b9ba5.ttf"
   },
   {
-    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
-    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
-  },
-  {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
     "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
@@ -120,7 +120,7 @@ self.__precacheManifest = [
     "url": "/static/media/brand-icons.a1a749e8.svg"
   },
   {
-    "revision": "95c49003883d4c54221df274900782eb",
+    "revision": "838eff3e981c4d925249ddab855f9233",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.68f3127a6aea4ef8386acbbc98efde3c.js"
+  "/precache-manifest.76df1bc12fe88e68727ca0a1052512c7.js"
 );
 
 workbox.clientsClaim();
