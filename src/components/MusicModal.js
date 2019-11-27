e92ca3291096e97d69321d9665b2acfe5c5e4118
Wes McLaughlin <wesley@remine.com>
2019-11-27 17:32:04 -0500

    merge

--- src/components/MusicModal.js
+++ src/components/MusicModal.js
@@ -2,29 +2,29 @@ import React from 'react'
 import { Header } from 'semantic-ui-react'
 
 
-let linkStyle = { margin: '10px', display:'flex', flexWrap:'nowrap', flexGrow: 1,  flexBasis: '100%'}
-let MusicContainer = { width: '100%', alignItems: 'center', display:'flex', flexWrap:'nowrap', justifyContent: 'center'}
+let linkStyle = { margin: '10px', display: 'flex', flexWrap: 'nowrap', flexGrow: 1, flexBasis: '100%' }
+let MusicContainer = { width: '100%', alignItems: 'center', display: 'flex', flexWrap: 'nowrap', justifyContent: 'center' }
 let MusicIcon = { width: '100%' }
 
-const mq = window.matchMedia( "(max-width: 570px)" );
+const mq = window.matchMedia("(max-width: 570px)");
 if (mq.matches) {
-    MusicContainer = { ...MusicContainer, flexWrap: 'wrap', height: '50vh', alignContent:'center' }
-    linkStyle = {...linkStyle, justifyContent: 'center'}
-    MusicIcon = { ...MusicIcon, width: 'auto', maxHeight: '10vh', alignContent:'center'}
+    MusicContainer = { ...MusicContainer, flexWrap: 'wrap', height: '50vh', alignContent: 'center' }
+    linkStyle = { ...linkStyle, justifyContent: 'center' }
+    MusicIcon = { ...MusicIcon, width: 'auto', maxHeight: '10vh', alignContent: 'center' }
 }
 
 
-const MusicModal = ({ links, header}) => ({
+const MusicModal = ({ links, header }) => ({
     header: <Header icon='headphones' content={header} />,
     content: (
-    <div style={MusicContainer}>
-        {
-            links.map(link => (
-            <a key={link.url} target="_blank" rel="noopener noreferrer" href={link.url} style={linkStyle}>
-                <img style={MusicIcon} src={link.icon} alt={link.alt}/>
-            </a>
-        ))}
-    </div>
+        <div style={MusicContainer}>
+            {
+                links.map(link => (
+                    <a key={link.url} target="_blank" rel="noopener noreferrer" href={link.url} style={linkStyle}>
+                        <img style={MusicIcon} src={link.icon} alt={link.alt} />
+                    </a>
+                ))}
+        </div>
     )
 })
 
--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -7,27 +7,27 @@ import MusicModal from '../components/MusicModal'
 import { MusicServices, SocialServices, SocialServicesInverted } from '../content/services'
 
 const SocialLinks = props => {
-    const iconStyle = {width:'5vw', minWidth:40, maxWidth:50, margin:'0 10px'}
+    const iconStyle = { width: '5vw', minWidth: 40, maxWidth: 50, margin: '0 10px' }
     const ss = props.inverted ? SocialServicesInverted : SocialServices
     const SocialElements = [
-        ...ss.links.slice(0,2),
-        {icon: social, alt: 'Connect with sleave', url: '', type:'modal', modal: MusicModal(SocialServicesInverted)},
-        {icon: headphones, alt: 'Listen to sleave', url: '', type:'modal', modal: MusicModal(MusicServices)}
+        ...ss.links.slice(0, 2),
+        { icon: social, alt: 'Connect with sleave', url: '', type: 'modal', modal: MusicModal(SocialServicesInverted) },
+        { icon: headphones, alt: 'Listen to sleave', url: '', type: 'modal', modal: MusicModal(MusicServices) }
     ]
 
     return (
         <div style={props.style}>
             {SocialElements.map(elem => (
-                elem.type === 'modal' ? 
-                <BasicModal key={elem.alt} modal={elem.modal}>
-                    <div>
-                        <img src={elem.icon} alt={elem.alt} style={iconStyle}/>
-                    </div>
-                </BasicModal>
-                :
-                <a key={elem.alt} href={elem.url}>
-                    <img src={elem.icon} alt={elem.alt} style={iconStyle}/>
-                </a>
+                elem.type === 'modal' ?
+                    <BasicModal key={elem.alt} modal={elem.modal} alt={elem.type}>
+                        <div>
+                            <img src={elem.icon} alt={elem.alt} style={iconStyle} />
+                        </div>
+                    </BasicModal>
+                    :
+                    <a key={elem.alt} href={elem.url}>
+                        <img src={elem.icon} alt={elem.alt} style={iconStyle} />
+                    </a>
             ))}
         </div>
     )
